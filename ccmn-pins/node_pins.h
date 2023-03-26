dc9a60c2ac27be284a3348375e12194cdd8823e2
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-26 17:31:58 -0400

    Add pin definitions and board.jsons

---
+++ ccmn-pins/node_pins.h
@@ -0,0 +1,20 @@
+#ifndef NODE_PINS_H
+#define NODE_PINS_H
+
+#ifdef NODE_BOARD_1V1
+#include "node-1.1.h"
+#endif
+
+#ifdef NODE_BOARD_1V2
+#include "node-1.2.h"
+#endif
+
+#ifdef NODE_BOARD_2V0A
+#include "node-2.0A.h"
+#endif
+
+#ifdef NODE_BOARD_2V0B
+#include "node-2.0B.h"
+#endif
+
+#endif
