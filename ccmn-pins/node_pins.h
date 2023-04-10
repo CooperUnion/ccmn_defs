b20a8d4bc30e0b7c2b589fdeafb785555727312c
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-04-10 14:24:45 -0400

    #error if no node pins were selected

--- ccmn-pins/node_pins.h
+++ ccmn-pins/node_pins.h
@@ -3,18 +3,27 @@
 
 #ifdef NODE_BOARD_1V1
 #include "node-1.1.h"
+#define NODE_BOARD_PINS_DEFINED
 #endif
 
 #ifdef NODE_BOARD_1V2
 #include "node-1.2.h"
+#define NODE_BOARD_PINS_DEFINED
 #endif
 
 #ifdef NODE_BOARD_2V0A
 #include "node-2.0A.h"
+#define NODE_BOARD_PINS_DEFINED
 #endif
 
 #ifdef NODE_BOARD_2V0B
 #include "node-2.0B.h"
+#define NODE_BOARD_PINS_DEFINED
+#endif
+
+
+#ifndef NODE_BOARD_PINS_DEFINED
+#error "No node board pins were selected; check that NODE_BOARD_XXXX is defined."
 #endif
 
 #endif
